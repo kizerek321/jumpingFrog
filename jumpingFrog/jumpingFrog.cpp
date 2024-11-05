f29a7fc8c35bab8db40a518671e80f668bef06fb
kizer <krzys.szudy@gmail.com>
2024-11-05 21:22:55 +0100

    printing board function

--- jumpingFrog/jumpingFrog.cpp
+++ jumpingFrog/jumpingFrog.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include "conio2.h"
 
+#define START_BOARD 5
+
+
+
+
 
 typedef struct Frog {
     int x = 0, y = 0;
@@ -29,9 +34,9 @@ typedef struct Car {
 
 typedef struct Board {
 	int width = 0, height = 0;
-	int roadHeight = 0, roadWidth = 0;
 	int numberOfStreets = 0;
 	int numberOfCars = 0; // number of cars on the street
+	int * x_road , *y_road ;
 }Board;
 
 void loadFile (Board*board,Frog*frog,Car*car) {
@@ -41,11 +46,9 @@ void loadFile (Board*board,Frog*frog,Car*car) {
 		exit ( 1 );
 	}
 	fscanf ( file , 
-			 "%d %d %d %d %d %d %d %d %d %d %d %d %d %c" ,
+			 "%d %d %d %d %d %d %d %d %d %d %d %c %c" ,
 			 &board->width , 
 			 &board->height, 
-			 &board->roadHeight ,
-			 &board->roadWidth ,
 			 &board->numberOfStreets ,
 			 &board->numberOfCars ,
 			 &frog->x ,
@@ -55,8 +58,24 @@ void loadFile (Board*board,Frog*frog,Car*car) {
 			 &frog->jump ,
 			 &frog->jumpHeight ,
 			 &frog->colision ,
-			 &frog->sign );
+			 &frog->sign, 
+			 &car->sign);
 	fclose ( file );
+}
+
+void printingBoard (Board&board, Frog&frog, Car&car) {
+	int y = START_BOARD + board.height;
+	int x = START_BOARD + board.width;
+	int *
+	for ( int i = START_BOARD; i < y; i++ ) {
+		for ( int j = START_BOARD; j < x; j++ ) {
+			gotoxy ( i , j );
+			if(i == START_BOARD || i == y - 1 || j == START_BOARD || j == x - 1){
+				printf ( "*" );
+			}
+		}
+	}
+
 }
 void startingStage (Board &board, Frog&frog) {
 
@@ -64,6 +83,7 @@ void startingStage (Board &board, Frog&frog) {
 
 int main()
 {
+	srand ( time ( NULL ) );
 	Board board;
 	Frog frog;
 	Car car;
@@ -71,6 +91,11 @@ int main()
 	_setcursortype ( _NOCURSOR );
 	textmode ( C4350 );
 	loadFile ( &board , &frog , &car);
+	board.x_road = new int [board.numberOfStreets];
+	board.y_road = new int[board.numberOfStreets];
+	for ( int i = 0; i < board.numberOfStreets; i++ ) {
+		board.x_road[i] = board.y_road[i] = rand () % 3 + 1;
+	}
 	char input = 'a';
 	while ( input != 'q' ) {
 		clrscr ();
@@ -81,4 +106,6 @@ int main()
 		std::cout << frog.sign;
 		input = getch ();
 	}
+	delete[] board.x_road;
+	delete[] board.y_road;
 }
